c7963c86397ebcc4d82aa658b43984f8ffdd0c91
AmanBisht01 <bishtaman1111@gmail.com>
2022-03-08 22:20:00 +0530

    frontend

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,46 @@
-import React from 'react'
-import './Home.css'
-import Navigation from './Navigation'
-import AppBody from './AppBody'
+import React from "react";
+import "./Home.css";
+import Navigation from "./Navigation";
+import AppBody from "./AppBody";
 
 import axios from "axios";
 import { io } from "socket.io-client";
 import { useNavigate } from "react-router-dom";
-const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [roomId, setroomId] = useState("");
+// const Home = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [roomId, setroomId] = useState("");
 
-  function toggleModal() {
-    setIsOpen(!isOpen);
-  }
-  const getRoomID = (event) => {
-    event.preventDefault();
-  };
-  // const socket = io("http://localhost:8000");
+//   function toggleModal() {
+//     setIsOpen(!isOpen);
+//   }
+//   const getRoomID = (event) => {
+//     event.preventDefault();
+//   };
+//   // const socket = io("http://localhost:8000");
 
-  // socket.on("connect", () => {
-  //   console.log("Socket connected");
-  // });
+//   // socket.on("connect", () => {
+//   //   console.log("Socket connected");
+//   // });
 
-  const navigate = useNavigate();
-  async function joinRoom() {
-    await axios
-      .get("http://localhost:5000/join", {
-        params: {
-          roomId: roomId,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        navigate("room=/" + `${res.data}`);
-      });
-  }
+//   const navigate = useNavigate();
+//   async function joinRoom() {
+//     await axios
+//       .get("http://localhost:5000/join", {
+//         params: {
+//           roomId: roomId,
+//         },
+//       })
+//       .then((res) => {
+//         console.log(res.data);
+//         navigate("room=/" + `${res.data}`);
+//       });
+//   }
 
-  async function createRoom(e) {
-    await axios.get("http://localhost:5000/room").then((res) => {
-      // console.log(res.data);
-      navigate("room=/" + `${res.data.room}`);
-    });
-  }
-
+//   async function createRoom(e) {
+//     await axios.get("http://localhost:5000/room").then((res) => {
+//       navigate("room=/" + `${res.data.room}`);
+//     });
+//   }
 
 const Home = () => {
   return (
@@ -50,7 +48,7 @@ const Home = () => {
       <Navigation />
       <AppBody />
     </div>
-  )
-}
-
-export default Home
+  );
+};
+// };
+export default Home;
